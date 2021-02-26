365740c3fc2efbbe95ba2b76ec1f76e0e98d75be
tajmahak <tajmahal@bk.ru>
2021-02-26 10:56:29 +1000

    поправки

--- ImageShakalizer.cs
+++ ImageShakalizer.cs
@@ -9,7 +9,7 @@ namespace DCT
         public static Image Damage(Bitmap srcImage, int quality) {
             byte[,,] rgb = BitmapToByteRgb(srcImage);
             byte[,,] ycbcr = ByteRgbToByteYCbCr(rgb);
-            //Quantization(ycbcr, quality);
+            Quantization(ycbcr, quality);
             byte[,,] result_rgb = ByteYCbCrToByteRgb(ycbcr);
             Bitmap result_image = ByteRgbToBitmap(result_rgb);
 
@@ -83,9 +83,9 @@ namespace DCT
                     byte r = rgb[0, h, w];
                     byte g = rgb[1, h, w];
                     byte b = rgb[2, h, w];
-                    ycbcr[0, h, w] = GetByte(0.0 + (0.299 * r) + (0.587 * g) + (0.114 * b));
-                    ycbcr[1, h, w] = GetByte(128.0 - (0.168736 * r) - (0.331264 * g) + (0.5 * b));
-                    ycbcr[2, h, w] = GetByte(128.0 + (0.5 * r) - (0.418688 * g) - (0.081312 * b));
+                    ycbcr[0, h, w] = ToByte((0.299 * r) + (0.587 * g) + (0.114 * b));
+                    ycbcr[1, h, w] = ToByte(128.0 - (0.168736 * r) - (0.331264 * g) + (0.5 * b));
+                    ycbcr[2, h, w] = ToByte(128.0 + (0.5 * r) - (0.418688 * g) - (0.081312 * b));
                 }
             }
             return ycbcr;
@@ -100,17 +100,20 @@ namespace DCT
                     byte y = ycbcr[0, h, w];
                     byte cb = ycbcr[1, h, w];
                     byte cr = ycbcr[2, h, w];
-                    rgb[0, h, w] = GetByte(y + 1.402 * (cr - 128.0));
-                    rgb[1, h, w] = GetByte(y - 0.34414 * (cb - 128.0) - 0.71414 * (cr - 128.0));
-                    rgb[2, h, w] = GetByte(y + 1.772 * (cb - 128.0));
+                    rgb[0, h, w] = ToByte(y + 1.402 * (cr - 128.0));
+                    rgb[1, h, w] = ToByte(y - 0.34414 * (cb - 128.0) - 0.71414 * (cr - 128.0));
+                    rgb[2, h, w] = ToByte(y + 1.772 * (cb - 128.0));
                 }
             }
             return rgb;
         }
 
         private static void Quantization(byte[,,] ycbcr, int quantizator) {
+            double[,] dct_matrix = GetDCTMatrix(8.0);
             byte[,] q_matrix = GetQuantMatrix(quantizator);
-            double[,] dct = new double[8, 8];
+
+            double[,] t_matrix_cb = new double[8, 8];
+            double[,] t_matrix_cr = new double[8, 8];
 
             int heigth = ycbcr.GetLength(1);
             int width = ycbcr.GetLength(2);
@@ -129,18 +132,48 @@ namespace DCT
                             int offset_h = h - block_h;
                             int offset_w = w - block_w;
 
+                            //byte cb = ycbcr[1, h, w];
+                            //byte cr = ycbcr[2, h, w];
 
-                            //ycbcr[0, h, w] = q_matrix[offset_h, offset_w];
-                            //ycbcr[1, h, w] = q_matrix[offset_h, offset_w];
-                            //ycbcr[2, h, w] = q_matrix[offset_h, offset_w];
+                            //t_matrix_cb[offset_h, offset_w] = cb * dct_matrix[offset_w, offset_h];
+                            //t_matrix_cr[offset_h, offset_w] = cr * dct_matrix[offset_w, offset_h];
+
+                            //t_matrix_cb[offset_h, offset_w] *= dct_matrix[offset_h, offset_w];
+                            //t_matrix_cr[offset_h, offset_w] *= dct_matrix[offset_h, offset_w];
+
+                            //t_matrix_cb[offset_h, offset_w] *= q_matrix[offset_h, offset_w];
+                            //t_matrix_cr[offset_h, offset_w] *= q_matrix[offset_h, offset_w];
+
+                            //ycbcr[1, h, w] = GetByte(t_matrix_cb[offset_h, offset_w]);
+                            //ycbcr[2, h, w] = GetByte(t_matrix_cr[offset_h, offset_w]);
 
 
-                            { }
-
+                            ycbcr[1, h, w] = 0;
+                            ycbcr[2, h, w] = 0;
                         }
+                    }
+
+
+
+                    { }
+
+
+                }
+            }
+        }
+
+        private static double[,] GetDCTMatrix(double n) {
+            double[,] dct_matrix = new double[8, 8];
+            for (int i = 0; i < 8; i++) {
+                for (int j = 0; j < 8; j++) {
+                    if (i == 0) {
+                        dct_matrix[i, j] = 1.0 / Math.Sqrt(n);
+                    } else {
+                        dct_matrix[i, j] = Math.Sqrt(2.0 / n) * Math.Cos((2.0 * j + 1.0) * i * Math.PI / (2.0 * n));
                     }
                 }
             }
+            return dct_matrix;
         }
 
         private static byte[,] GetQuantMatrix(int q) {
@@ -153,7 +186,9 @@ namespace DCT
             return q_matrix;
         }
 
-        private static byte GetByte(double value) {
+
+
+        private static byte ToByte(double value) {
             if (value <= 0.0) {
                 return 0;
             } else if (value >= 255.0) {
